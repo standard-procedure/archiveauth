578788b1cc9f71bf874a54535a7dc31c27045cbc
rahoulb <rahoulb@echodek.co>
2023-03-04 21:55:41 +0000

    Config for URLs

--- config.ts
+++ config.ts
@@ -4,15 +4,15 @@ import { TypeInput } from "supertokens-node/types";
 import Dashboard from "supertokens-node/recipe/dashboard";
 
 export function getApiDomain() {
-  const apiPort = process.env.REACT_APP_API_PORT || 3001;
-  const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+  const apiPort = process.env.AUTH_API_PORT || 3001;
+  const apiUrl = process.env.AUTH_API_URL || `http://localhost:${apiPort}`;
   return apiUrl;
 }
 
 export function getWebsiteDomain() {
-  const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+  const websitePort = process.env.AUTH_WEBSITE_PORT || 3000;
   const websiteUrl =
-    process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    process.env.AUTH_WEBSITE_URL || `http://localhost:${websitePort}`;
   return websiteUrl;
 }
 
